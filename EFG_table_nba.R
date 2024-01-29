65573cb4ff94e154a6e17557bb487ca0a5ecb005
AlexV <149329570+AlexVikatos@users.noreply.github.com>
2024-01-29 20:32:17 +0200

    EFG_table_nba.R

--- EFG_table_nba.R
+++ EFG_table_nba.R
@@ -1,7 +1,5 @@
 #Author: Alex Vikatos
 
-
-
 library(tidyverse)
 library(glue)
 library(gt)
@@ -9,7 +7,9 @@ library(gtExtras)
 library(hoopR)
 
 df = load_nba_player_box(seasons = most_recent_nba_season())
+str(df)
 df = df[, c(6, 7, 9, 12:18, 28, 36, 42)]
+
 
 df1 = df %>%
   filter(minutes > 20) %>%
@@ -23,8 +23,11 @@ df1 = df %>%
     total_three_point_field_goals_made = sum(three_point_field_goals_made),
     total_free_throws_attempted = sum(free_throws_attempted),
     total_free_throws_made = sum(free_throws_made),
-    EFG = (total_field_goals_made + 0.5 * total_three_point_field_goals_made) / (total_field_goals_attempted + total_three_point_field_goals_attempted)
-  ) 
+  ) %>%
+  mutate(total_two_point_made=total_field_goals_made-total_three_point_field_goals_made)%>%
+  mutate(EFG = (total_two_point_made + 1.5 * total_three_point_field_goals_made) / (total_field_goals_attempted))
+
+str(df1)
 
 # Table creation
 df1 %>%
@@ -36,11 +39,10 @@ df1 %>%
   head(10) %>%
   gt() %>%
   gt_img_rows(columns = 2, height = 25) %>%
-  fmt_number(columns = c(EFG),decimals = 2)%>%
-  tab_footnote(footnote="Author: Alex Vikatos")%>%
+  fmt_number(c(EFG),decimals = 2)%>%
   cols_label(athlete_display_name = "Name", EFG = "EFG",athlete_id="",total_points="TOTAL POINTS") %>%
-  tab_header(title = "Exploring the EFG of the Top-10 NBA Scorers",subtitle = "Data:hoopR/NBAstats") %>%
-  tab_options(data_row.padding = px(10), heading.title.font.size = 18, heading.subtitle.font.size = 16, table.font.size = 15)%>%
-  tab_style(style=list(cell_text(weight = "bold")),locations = cells_body(columns=c(athlete_display_name,EFG,total_points)))
-                                                   
+  tab_header(title = "Exploring the EFG of the Top-10 NBA Scorers") %>%
+  tab_footnote(footnote = "Data:hoopR/NBAstats")%>%
+  tab_style(style = list(cell_text(weight="bold")),locations = cells_body(columns=c(athlete_display_name,EFG,total_points)))%>%
+  tab_options(data_row.padding = px(10), heading.title.font.size = 18, heading.subtitle.font.size = 16, table.font.size = 15)
 
